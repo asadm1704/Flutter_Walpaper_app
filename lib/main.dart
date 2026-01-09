ae86a998075e80bb8d1a5b34a30a18ffdcb380d6
Asad Wazir <asadm1704@gmail.com>
2026-01-09 19:27:18 +0100

    Update app name to 'AM Wallpapers' and set custom app icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class WallpaperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wallpaper App',
+      title: 'AM Wallpapers',
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
